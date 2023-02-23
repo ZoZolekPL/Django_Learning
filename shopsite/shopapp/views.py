f9ce4e2ffe7bcaa07b5b6a7c5a66f804f8dd9d3f
ZoZolekPL <simon334.pl@gmail.com>
2023-02-23 21:33:55 +0100

    lesson1

--- shopsite/shopapp/views.py
+++ shopsite/shopapp/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Categories, Products
 
 # Create your views here.
+
+def product_list(request, category_slug=None):
+    category = None
+    categories = Categories.objects.all()
+    products = Products.objects.filter(available=True)
+    if category_slug:
+        category = get_object_or_404(Categories, slug = category_slug)
+        products = products.filter(category=category)
+    return  render(request, '')
