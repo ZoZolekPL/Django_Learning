82762c352226061e5c1f2afea48d90cfe53dbbf4
ZoZolekPL <simon334.pl@gmail.com>
2023-02-28 18:12:58 +0100

    1

--- shopsite/shopapp/urls.py
+++ shopsite/shopapp/urls.py
@@ -6,5 +6,5 @@ app_name = 'shopapp'
 urlpatterns = [
     path('', views.product_list, name='product_list'),
     path('<slug:category_slug>/', views.product_list, name='products_list_by_category'),
-    path('<imt:id>/slug:slug/', views.products_details, name= 'products_detail'),
+    path('<int:id>/slug:slug/', views.products_details, name= 'products_detail'),
 ]
